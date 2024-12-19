bee56361a7d7e946314462a7249e4668a78a9dc8
amasini0 <a.masini@cineca.it>
2024-12-19 12:58:20 +0100

    Complete section 1 and 2
    * Add solutions to exercises/01_intro
    
    * Add solutions to exercises/02_intro
      - Change main Cargo.toml for exercise 08_overflow

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,14 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    if n < 2 { return 1; }
+    let mut current = n;
+    let mut result = 1;
+    while current >= 2 {
+       result *= current; 
+       current -= 1;
+    }
+    result
 }
 
 #[cfg(test)]
--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,11 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    if n < 2 { return 1; }
+    let mut result = 1;
+    for i in 2..=n {
+        result *= i;
+    }
+    result
 }
 
 #[cfg(test)]
